9968325d50e049b8a35609ed8cd50e04976a8e12
ydv129 <ydv129111@gmail.com>
2026-04-05 19:53:11 +0530

    finalized

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Suspense, lazy, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import RouterScrollTop from "./components/ScrollToTop/RouterScrollTop";
 
 const Home = lazy(() => import("./pages/Home/HomePage"));
 const About = lazy(() => import("./pages/About/AboutPage"));
@@ -9,8 +10,6 @@ const Services = lazy(() => import("./pages/Service/ServicesPage"));
 const Project = lazy(() => import("./pages/Project/ProjectPage"));
 const ProjectApp = lazy(() => import("./pages/Project/ProjectApp"));
 const ProjectGame = lazy(() => import("./pages/Project/ProjectGame"));
-
-import RouterScrollTop from "./components/ScrollToTop/RouterScrollTop";
 
 function App() {
   useEffect(() => {
@@ -35,10 +34,10 @@ function App() {
         }
       >
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/service" element={<Services />}></Route>
-          <Route path="/project" element={<Project />}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/service" element={<Services />} />
+          <Route path="/project" element={<Project />} />
           <Route path="/project/app" element={<ProjectApp />} />
           <Route path="/project/game" element={<ProjectGame />} />
         </Routes>
